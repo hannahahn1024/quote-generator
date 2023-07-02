06503a8dffd448314ae6e1897f36d54c6129fc7c
Hannah <hannahjahn1024@gmail.com>
2023-07-01 19:58:17 -0700

    visuals

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>Quote Generator</h1>
+      <section>
+        <button>New Quote</button>
+      </section>
+    </div>
+  );
+}
+
+export default App;
